fdecb3eb262246e4ace16a4be3d0962aae38c185
neda <nedaghezavati@gmail.com>
2021-05-04 22:02:19 +0430

    orders implementation

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OrdersController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'total_preparation_time' => 'required|string',
+            'total_price' => 'required|integer',
+            'counts' => 'required',
+        ]);
+
+        $order = new Order();
+        $order->user_id = Auth::id();
+    }
 }
